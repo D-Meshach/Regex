3f6b90685d5aeeecbe27c97dd18b6bc94d2cbba8
D-Meshach <meshachdaniel456@gmail.com>
2023-03-27 22:27:25 +0530

    Welcome

---
+++ regex_demo/Registration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace regex_demo
+{
+    internal class Registration
+    {
+        public Registration()
+        {
+            Console.WriteLine("Username=Mahesh"+" Match="+RegexRegistration.ValidUser("Mahesh"));
+            
+        }
+    }
+    public class RegexRegistration
+    {
+
+        
+        public  static bool ValidUser(String name) {
+            String RegEx = "^[A-Z][A-Za-z]{3,}$";
+            
+            return Regex.IsMatch(name, RegEx);
+
+        }
+      
+      
+    
+    }
+
+}
